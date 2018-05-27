e9d323238165601bf48d653c01775a923a210650
Albert Stanula <albertstanula97@gmail.com>
2018-05-27 15:25:46 +0200

    Project created.

---
+++ app/src/main/java/pk/dietmonitor/MainWindow.java
@@ -0,0 +1,13 @@
+package pk.dietmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_window);
+    }
+}
